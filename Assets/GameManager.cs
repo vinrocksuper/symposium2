daf75a5da98950f921e3d472bc5981e3b77e89c4
Vincent <vli8558@bths.edu>
2019-03-12 16:26:57 -0400

    I wanna die

---
+++ Assets/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public DigitalClock dc;
+    public Text date;
+    void Update()
+    {
+
+        if (dc.hours == 12 && dc.am && dc.minutes == 0)
+        {
+            if(dc.day< 10)
+            {
+                date.text = dc.season + "/0" + dc.day;
+            }
+            else
+            {
+                date.text = dc.season + "/" + dc.day;
+            }
+            
+        }
+    }
+    public void UpdateTime()
+    {
+        if (dc.day < 10)
+        {
+            date.text = dc.season + "/0" + dc.day;
+        }
+        else
+        {
+            date.text = dc.season + "/" + dc.day;
+        }
+    }
+}
