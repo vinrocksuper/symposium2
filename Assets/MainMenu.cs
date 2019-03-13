b8ab54c372dba161db56459b1a1214e65f46a850
a <a>
2019-03-13 09:51:17 -0400

    a
    a

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -14,4 +14,8 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit");
         Application.Quit();
     }
+    public void ReturnMM()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
 }
